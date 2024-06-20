64f2c2f16883cf45352f32bc131ad2dd3a563954
Jon Cheng <joncheng.dev@gmail.com>
2024-06-19 23:12:43 -0700

    WIP: add model for nutrition goals

---
+++ NutritionGoals/Models/Goal.cs
@@ -0,0 +1,9 @@
+namespace NutritionGoalsAPI.Models
+{
+  public class Goal
+  {
+    public int GoalId { get; set; }
+    public int Protein { get; set; }
+    public int Carbohydrate { get; set; }
+  }
+}
